6bb50c55b7a24e830f2762927c93c99dce90edc6
ecsystems-team ecsystems-team <ecsystems-team@outlook.com>
2018-02-13 17:19:33 +0000

    testing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-#output "location" {
-#  value = "${azurerm_resource_group.resource_group.location}"
-#}
-#
-#output "resource_group_name" {
-#  value = "${azurerm_resource_group.resource_group.name}"
-#}
+output "location" {
+  value = "${azurerm_resource_group.resource_group.location}"
+}
+
+output "resource_group_name" {
+  value = "${azurerm_resource_group.resource_group.resource_group_name}"
+}
